d92f9dd387c7e281f57b7e179f30c060eb454e52
void07 <krojas96ce@gmail.com>
2021-07-18 22:17:19 -0600

    bug fixes

--- Ahorcado/src/game/Game.java
+++
@@ -1,9 +0,0 @@
-package game;
-public class Game {
-    private String[] palabras = null;
-
-    public Game() {
-        this.palabras = new String[]{"pecera", "perro", "salchicha", "nube", "globo", "aire"};
-    }
-
-}
